32d9f63c8242febb53c698c7ad4bec8f0a3e805f
Arijeet Baruah <rishithegamecreator@protonmail.ch>
2016-07-24 13:22:04 +0530

    wordpress added

--- index.php
+++ index.php
@@ -1,47 +1,17 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<title>login</title>
-<script>
-	function validate() {
-    	var x = document.forms["MyLogin"]["usr"].value;
-		var p = document.forms["MyLogin"]["pwd"].value;
-    	if (x == null || x == "") {
-    	    alert("Name must be filled out");
-    	    return false;
-    	}
-		if (p == null || p == "") {
-    	    alert("Password must be filled out");
-    	    return false;
-    	}
-	}
-</script>
-</head>
-<body>
-<div class="row">
-  <div class="col-sm-4"> </div>
-  <div class="col-sm-4"> 
-  <form action="login.php" name="MyLogin" onSubmit="return validate();" method="post">
-  	<div class="form-group">
-  		<label for="usr">Name:</label>
-  		<input type="text" name="usr" class="form-control">
-	</div>
-	<div class="form-group">
-  		<label for="pwd">Password:</label>
-  		<input type="password" name="pwd" class="form-control">
-	</div>
-    <div class="form-group">
-  		<input type="submit" value="Login" class="btn btn-primary">
-        <button type="reset" class="btn btn-primary">Reset</button>
-	</div>
-    </form>
-  </div>
-  <div class="col-sm-4"> </div> 
-</div>
-</body>
-</html>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
